acd3f95bc0fe7388d6e74cdf5a645fb6ab885adb
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-10-19 13:13:52 +0900

    testing around with styles and dummyText

--- App.jsx
+++ App.jsx
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Hello World!!</Text>
+        <Text style={styles.dummyText}>Hello World!!</Text>
       </View>
-      <Text>Hello World22222!!</Text>
+      <Text style={styles.dummyText}>Hello World22222!!</Text>
       <Button title="여기에 버튼이름" />
     </View>
   );
@@ -19,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 16,
   },
 });
